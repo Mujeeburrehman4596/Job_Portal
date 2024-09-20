c23d29d009ba76142d700339d953dc24e0bc75fd
Mujeeburrehman4596 <mujeeburrehman4596@gmail.com>
2024-09-20 12:54:58 +0500

    added job files

---
+++ job.js
@@ -0,0 +1,7 @@
+
+function showJobDetails(title, description, location, type) {
+    document.getElementById('jobTitle').innerText = title;
+    document.getElementById('jobDescription').innerText = description;
+    document.getElementById('jobLocation').innerText = location;
+    document.getElementById('jobType').innerText = type;
+}
